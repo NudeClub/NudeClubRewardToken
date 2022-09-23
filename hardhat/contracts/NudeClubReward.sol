17361f964dbccbe386e1943f04cd0bd1e1720de6
taylorferran <tailzpk@hotmail.co.uk>
2022-09-23 16:22:59 +0100

    Start unit testing

--- hardhat/contracts/NudeClubReward.sol
+++ hardhat/contracts/NudeClubReward.sol
@@ -22,40 +22,49 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠀⠀⠀⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  */
 
 contract NudeClubReward is ERC721Enumerable, Ownable {
-    uint256 public maxTokens;
+
+    uint256 public amountMinted;
     address public nudeClubRewardContract;
     string _baseTokenURI;
     mapping(address => bool) rewardRedeemable;
+    bool paused = true;
+
+    modifier IsPaused() {
+        require(!paused, "contract paused");
+        _;
+    }
 
     constructor(string memory baseURI) ERC721("Nude Club Reward Token", "NUDE") {
         _baseTokenURI = baseURI;
     }
 
-    function mint() public payable {
-        require(maxTokens < 500, 'None left');
-        require(msg.sender == tx.origin, 'EOAs only');
+    function mint() public payable IsPaused {
+        require(amountMinted < 500, "None left");
+        require(msg.sender == tx.origin, "EOAs only");
+        require(balanceOf(msg.sender) == 0, "One per address");
         require(!rewardRedeemable[msg.sender], "Address already ready to redeem reward");
-        require(balanceOf(msg.sender) == 0, 'One per address');
-        require(msg.value == 0.01 ether, 'Wrong value');
-        ++maxTokens;
+        require(msg.value == 0.002 ether, "Wrong value");
+        ++amountMinted;
         rewardRedeemable[msg.sender] = true;
-        _safeMint(msg.sender, maxTokens);
+        _safeMint(msg.sender, amountMinted);
     }
 
     // Reward function defined in the future will call this function to burn the NFT
     // and remove the address from the award list. If the function returns true to our
     // reward function we can proceed and reward the address
-    function redeemReward(address holder, uint tokenID) public returns (bool) {
-        require(msg.sender == nudeClubRewardContract, 'To be called to redeem reward');
-        require(ownerOf(tokenID) == holder);
-        require(rewardRedeemable[msg.sender]);
-        _burn(tokenID);
-        rewardRedeemable[msg.sender] = false;
+    function redeemReward(address holder) public IsPaused returns (bool) {
+        require(msg.sender == nudeClubRewardContract, "To be called to redeem reward");
+        require(rewardRedeemable[holder]);
+        rewardRedeemable[holder] = false;
         return true;
     }
 
     function setNudeClubRewardContract(address newAddress) public onlyOwner {
         nudeClubRewardContract = newAddress;
+    }
+
+    function setPause(bool _paused) public onlyOwner {
+        paused = _paused;
     }
 
     function withdrawAll() public onlyOwner {
---
+++ hardhat/test/NudeClubRewardTest.js
@@ -0,0 +1,126 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+// We're deploying a new contract and addresses in each test here
+// This is only to test the basic functionally so far
+// I've used dummy IPFS metadata here, we want to replace that with a real test set of 500 reward tokens
+
+describe("Nude Club Reward contract", function () {
+  it("User can mint NFT after mint started and number minted increments correctly", async function () {
+
+    const [addr1] = await ethers.getSigners();
+
+    const NudeClubReward = await ethers.getContractFactory("NudeClubReward");
+
+    // Deploy contract with dummy metadata (This will be the ipfs link for the collection)
+    const NudeClubRewardContract = await NudeClubReward.deploy("ipfs://QmSyrVNtDaXoEDEEBa6uuYVTFfPmWoLNGmgDhoU9KkPpha/");
+
+    // Only owner can unpause the contract
+    await NudeClubRewardContract.setPause(false);
+
+    // Confirm no passes minted yet
+    expect(await NudeClubRewardContract.amountMinted()).to.equal(0);
+
+    // Happy path - owner successfully mints 1 nude club pass for this collection
+    await NudeClubRewardContract.connect(addr1).mint({ value: ethers.utils.parseEther("0.002") });
+
+    // Confirm one pass minted
+    expect(await NudeClubRewardContract.amountMinted()).to.equal(1);
+
+  });
+
+  it("User can only mint one NFT", async function () {
+
+    const [addr1] = await ethers.getSigners();
+
+    const NudeClubReward = await ethers.getContractFactory("NudeClubReward");
+
+    // Deploy contract with dummy metadata (This will be the ipfs link for the collection)
+    const NudeClubRewardContract = await NudeClubReward.deploy("ipfs://QmSyrVNtDaXoEDEEBa6uuYVTFfPmWoLNGmgDhoU9KkPpha/");
+
+    // Only owner can unpause the contract
+    await NudeClubRewardContract.setPause(false);
+
+    // Confirm no passes minted yet
+    expect(await NudeClubRewardContract.amountMinted()).to.equal(0);
+
+    // Happy path - owner successfully mints 1 nude club pass for this collection
+    await NudeClubRewardContract.connect(addr1).mint({ value: ethers.utils.parseEther("0.002") });
+
+    // Confirm one pass minted
+    expect(await NudeClubRewardContract.amountMinted()).to.equal(1);
+
+    // Contract should revert transaction
+    await expect( 
+        NudeClubRewardContract.connect(addr1).mint({ value: ethers.utils.parseEther("0.002") })
+    ).to.be.revertedWith("One per address");
+
+  });
+
+  it("User can only mint for 0.002 eth", async function () {
+
+    const [addr1, addr2] = await ethers.getSigners();
+
+    const NudeClubReward = await ethers.getContractFactory("NudeClubReward");
+
+    // Deploy contract with dummy metadata (This will be the ipfs link for the collection)
+    const NudeClubRewardContract = await NudeClubReward.deploy("ipfs://QmSyrVNtDaXoEDEEBa6uuYVTFfPmWoLNGmgDhoU9KkPpha/");
+
+    // Only owner can unpause the contract
+    await NudeClubRewardContract.setPause(false);
+
+    // Confirm no passes minted yet
+    expect(await NudeClubRewardContract.amountMinted()).to.equal(0);
+
+    // Happy path - owner successfully mints 1 nude club pass for this collection
+    await NudeClubRewardContract.connect(addr1).mint({ value: ethers.utils.parseEther("0.002") });
+
+    // Confirm one pass minted
+    expect(await NudeClubRewardContract.amountMinted()).to.equal(1);
+
+    // Contract should revert transaction
+    await expect( 
+        NudeClubRewardContract.connect(addr2).mint({ value: ethers.utils.parseEther("1") })
+    ).to.be.revertedWith("Wrong value");
+
+    // Contract should revert transaction
+    await expect( 
+        NudeClubRewardContract.connect(addr2).mint({ value: ethers.utils.parseEther("0.001") })
+    ).to.be.revertedWith("Wrong value");
+
+  });
+
+
+  it("500 passes minted to 500 addresses works correctly", async function () {
+
+    // TODO **
+
+    // Get signers for 500 addresses
+    //for(let i=0; i < 500; i++) {
+    //    string 
+    //    const [addr+i] = await ethers.getSigners();
+   // }
+   //const [addr+i, addr2] = await ethers.getSigners();
+/*
+   const NudeClubReward = await ethers.getContractFactory("NudeClubReward");
+
+    // Deploy contract with dummy metadata (This will be the ipfs link for the collection)
+    const NudeClubRewardContract = await NudeClubReward.deploy("ipfs://QmSyrVNtDaXoEDEEBa6uuYVTFfPmWoLNGmgDhoU9KkPpha/");
+
+    // Only owner can unpause the contract
+    await NudeClubRewardContract.setPause(false);
+
+    // Confirm no passes minted yet
+    expect(await NudeClubRewardContract.amountMinted()).to.equal(0);
+
+    // Mint a reward token to each address 
+    for(let i=0; i < 500; i++) {
+        await NudeClubRewardContract.connect(i).mint({ value: ethers.utils.parseEther("0.002") });
+    }
+
+    // Confirm one pass minted
+    expect(await NudeClubRewardContract.amountMinted()).to.equal(500);
+  */
+  });
+
+});
